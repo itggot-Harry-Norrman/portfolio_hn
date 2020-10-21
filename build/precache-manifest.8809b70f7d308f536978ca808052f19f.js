6a8d4e7741296994bd71f61c8fd62f2a46eb2e0d
harrynorrman <harry.norrman.5315@student.lu.se>
2020-10-21 14:51:56 +0200

    build also

---
+++ build/precache-manifest.8809b70f7d308f536978ca808052f19f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4143c773c5fb9a4e013b43a44c262af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "490456aa7b30dace788a",
+    "url": "/static/css/main.c1c3c721.chunk.css"
+  },
+  {
+    "revision": "fde0f6ed10c7a5036864",
+    "url": "/static/js/2.cae2a95e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.cae2a95e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "490456aa7b30dace788a",
+    "url": "/static/js/main.18a65175.chunk.js"
+  },
+  {
+    "revision": "0e4f09fa5ef72b831c0c",
+    "url": "/static/js/runtime-main.9abbe720.js"
+  },
+  {
+    "revision": "8625837a60ad14e4715c2908647e1342",
+    "url": "/static/media/NeueT.8625837a.otf"
+  },
+  {
+    "revision": "5866a1ac77aff7efa991b11c7b8da770",
+    "url": "/static/media/bg2.5866a1ac.png"
+  },
+  {
+    "revision": "24c553fb5f085aa65aa9dd993f927892",
+    "url": "/static/media/bg3.24c553fb.png"
+  }
+]);
